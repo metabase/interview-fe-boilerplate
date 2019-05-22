7c8016ef966d4ff445994192d914d7b568d447ce
Tom Robinson <tlrobinson@gmail.com>
2019-05-22 11:03:34 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-const App = props => <div>Hello world</div>;
+function App(props) {
+  return <div>Hello world</div>;
+}
 
 ReactDOM.render(<App />, document.getElementById("root"));
